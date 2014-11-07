b2f1ebb343fa49ee8c6389bc1333ac49e99074b5
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-11-07 11:12:55 +0200

    Hazelcast implementation for flix

--- src/main/groovy/com/sony/ebs/octopus3/microservices/flix/handlers/DeltaHandler.groovy
+++ src/main/groovy/com/sony/ebs/octopus3/microservices/flix/handlers/DeltaHandler.groovy
@@ -1,29 +1,32 @@
 package com.sony.ebs.octopus3.microservices.flix.handlers
 
+import com.sony.ebs.octopus3.commons.flows.FlowTypeEnum
+import com.sony.ebs.octopus3.commons.flows.ServiceTypeEnum
 import com.sony.ebs.octopus3.commons.process.ProcessIdImpl
 import com.sony.ebs.octopus3.commons.ratpack.file.ResponseStorage
 import com.sony.ebs.octopus3.commons.ratpack.handlers.HandlerUtil
+import com.sony.ebs.octopus3.commons.ratpack.handlers.HazelcastAwareDeltaHandler
 import com.sony.ebs.octopus3.commons.ratpack.product.cadc.delta.model.DeltaType
 import com.sony.ebs.octopus3.commons.ratpack.product.cadc.delta.model.RepoDelta
 import com.sony.ebs.octopus3.commons.ratpack.product.cadc.delta.validator.RequestValidator
 import com.sony.ebs.octopus3.microservices.flix.model.Flix
 import com.sony.ebs.octopus3.microservices.flix.model.ProductServiceResult
-import com.sony.ebs.octopus3.microservices.flix.services.basic.PackageService
 import com.sony.ebs.octopus3.microservices.flix.services.basic.DeltaService
+import com.sony.ebs.octopus3.microservices.flix.services.basic.PackageService
 import groovy.json.JsonOutput
 import groovy.util.logging.Slf4j
 import org.joda.time.DateTime
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import ratpack.groovy.handling.GroovyContext
-import ratpack.groovy.handling.GroovyHandler
 
 import static ratpack.jackson.Jackson.json
 
+@Slf4j(value = "log", category = "DeltaHandler")
 @Slf4j(value = "activity", category = "activity")
 @Component
 @org.springframework.context.annotation.Lazy
-class DeltaHandler extends GroovyHandler {
+class DeltaHandler extends HazelcastAwareDeltaHandler<RepoDelta> {
 
     @Autowired
     DeltaService deltaService
@@ -38,51 +41,61 @@ class DeltaHandler extends GroovyHandler {
     ResponseStorage responseStorage
 
     @Override
-    protected void handle(GroovyContext context) {
-        context.with {
-            RepoDelta delta = new RepoDelta(type: DeltaType.flixMedia, processId: new ProcessIdImpl(), publication: pathTokens.publication,
-                    locale: pathTokens.locale, sdate: request.queryParams.sdate, edate: request.queryParams.edate)
-            activity.info "starting {}", delta
+    FlowTypeEnum getFlow() {
+        FlowTypeEnum.FLIX
+    }
 
-            List productServiceResults = []
-            List errors = validator.validateRepoDelta(delta)
-            if (errors) {
-                activity.error "error validating {} : {}", delta, errors
-                response.status(400)
+    @Override
+    String getPublication(GroovyContext context) {
+        context.pathTokens.publication
+    }
 
-                def jsonResponse = json(status: 400, errors: errors, delta: delta)
+    @Override
+    String getLocale(GroovyContext context) {
+        context.pathTokens.locale
+    }
+
+    @Override
+    List flowValidate(GroovyContext context, RepoDelta delta) {
+        delta.sdate = context.request.queryParams.sdate
+        delta.edate = context.request.queryParams.edate
+        validator.validateRepoDelta(delta)
+    }
+
+    @Override
+    void flowHandle(GroovyContext context, RepoDelta delta) {
+        def startTime = new DateTime()
+        def flix = new Flix()
+
+        List productServiceResults = []
+        deltaService.processDelta(delta, flix).finallyDo({
+            if (delta.errors) {
+                activity.error "finished {} with errors: {}", delta, delta.errors
+                def endTime = new DateTime()
+                def timeStats = HandlerUtil.getTimeStats(startTime, endTime)
+                context.response.status(500)
+
+                def jsonResponse = json(status: 500, timeStats: timeStats, errors: delta.errors, delta: delta)
 
                 responseStorage.store(delta.processId.id, ["flix", "delta", delta.publication, delta.locale, delta.processId.id], JsonOutput.toJson(jsonResponse.object))
 
-                render jsonResponse
+                context.render jsonResponse
             } else {
-                def startTime = new DateTime()
-                def flix = new Flix()
-                deltaService.processDelta(delta, flix).finallyDo({
-                    if (delta.errors) {
-                        activity.error "finished {} with errors: {}", delta, delta.errors
-                        def endTime = new DateTime()
-                        def timeStats = HandlerUtil.getTimeStats(startTime, endTime)
-                        response.status(500)
-
-                        def jsonResponse = json(status: 500, timeStats: timeStats, errors: delta.errors, delta: delta)
-
-                        responseStorage.store(delta.processId.id, ["flix", "delta", delta.publication, delta.locale, delta.processId.id], JsonOutput.toJson(jsonResponse.object))
-
-                        render jsonResponse
-                    } else {
-                        handleFlixPackage(context, delta, flix, productServiceResults, startTime)
-                    }
-                }).subscribe({
-                    productServiceResults << it
-                    activity.debug "flix flow emitted: {}", it
-                }, { e ->
-                    delta.errors << HandlerUtil.getErrorMessage(e)
-                    activity.error "error in $delta", e
-                })
+                handleFlixPackage(context, delta, flix, productServiceResults, startTime)
             }
+        }).subscribe({
+            productServiceResults << it
+            activity.debug "flix flow emitted: {}", it
+        }, { e ->
+            delta.errors << HandlerUtil.getErrorMessage(e)
+            activity.error "error in $delta", e
+        })
+        true
+    }
 
-        }
+    @Override
+    RepoDelta createDelta(ProcessIdImpl processId, FlowTypeEnum flowTypeEnum, ServiceTypeEnum serviceTypeEnum, String publication, String locale) {
+        [processId: processId, flow: flowTypeEnum, service: serviceTypeEnum, publication: publication, locale: locale, type: DeltaType.flixMedia]
     }
 
     void handleFlixPackage(GroovyContext context, RepoDelta delta, Flix flix, List productServiceResults, DateTime startTime) {
